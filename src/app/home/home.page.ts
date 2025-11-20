cd8e71a83bb01670118881470a9bdd5533e6536a
Jadiel <saltossj09@gmail.com>
2025-11-20 18:09:40 -0500

    primera clase

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+})
+export class HomePage {
+  constructor() {}
+
+ message: string | null=null;
+
+ showMessage(){
+  this.message = "¡Felicidades! Has creado tu primera app con ionic";
+ }
+
+
+ 
+}
